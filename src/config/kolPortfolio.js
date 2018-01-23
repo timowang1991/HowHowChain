bf2860e4c131a10e3dcf609586d97c61fd912f1d
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-23 22:01:10 +0800

    Portfolio template ready

--- src/config/kolPortfolio.js
+++ src/config/kolPortfolio.js
@@ -1,32 +1,36 @@
 module.exports = {
     id: 1,
-    name: '',
+    name: '修敏傑',
     walletValue: 345.03,
     walletCurrency: 'ETH',
     kols: [
         {
             name: 'HowFun',
             image: 'https://imgur.com/a/tkBhi',
-            currencyName: 'HFC',
-            currencyToETH: 2.34
+            kolCurrencyValue: 100,
+            kolCurrencyName: 'HFC',
+            kolCurrencyToETH: 2.34
         },
         {
             name: '上班不要看NSFW',
             image: 'https://imgur.com/a/rbaMW',
-            currencyName: 'HFC',
-            currencyToETH: 2.34
+            kolCurrencyValue: 20,
+            kolCurrencyName: 'NSFWC',
+            kolCurrencyToETH: 1.62
         },
         {
             name: '囧星人',
             image: 'https://imgur.com/a/P8GQk',
-            currencyName: 'HFC',
-            currencyToETH: 2.34
+            kolCurrencyValue: 120,
+            kolCurrencyName: '9C',
+            kolCurrencyToETH: 200.37
         },
         {
             name: '黃阿瑪的後宮生活',
             image: 'https://imgur.com/a/wiI64',
-            currencyName: 'HFC',
-            currencyToETH: 2.34
+            kolCurrencyValue: 87,
+            kolCurrencyName: 'SCC',
+            kolCurrencyToETH: 1.67
         }
     ]
 }
