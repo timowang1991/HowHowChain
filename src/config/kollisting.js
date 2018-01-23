eb91091bf0fe19908a97b5574b6711afffdddb34
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-23 19:45:53 +0800

    add kollisting

---
+++ src/config/kollisting.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'HowFun',
+        description: '嗨大家安安! 我是HowHow 我喜歡拍影片記錄自己的生活',
+        image: 'https://yt3.ggpht.com/a-/AK162_4uyK8T6ToVUBK3DPltfnEribooJZ4ecv3CJA=s900-mo-c-c0xffffffff-rj-k-no',
+        totalMovies: 360,
+        ethValue: 23.34,
+        totalParticipants: 234766,
+        participants: [
+            {
+                image: 'https://yt3.ggpht.com/a-/AK162_4uyK8T6ToVUBK3DPltfnEribooJZ4ecv3CJA=s900-mo-c-c0xffffffff-rj-k-no'
+            },
+            {
+                image: 'https://yt3.ggpht.com/a-/AK162_4uyK8T6ToVUBK3DPltfnEribooJZ4ecv3CJA=s900-mo-c-c0xffffffff-rj-k-no'
+            },
+            {
+                image: 'https://yt3.ggpht.com/a-/AK162_4uyK8T6ToVUBK3DPltfnEribooJZ4ecv3CJA=s900-mo-c-c0xffffffff-rj-k-no'
+            },
+            {
+                image: 'https://yt3.ggpht.com/a-/AK162_4uyK8T6ToVUBK3DPltfnEribooJZ4ecv3CJA=s900-mo-c-c0xffffffff-rj-k-no'
+            },
+            {
+                image: 'https://yt3.ggpht.com/a-/AK162_4uyK8T6ToVUBK3DPltfnEribooJZ4ecv3CJA=s900-mo-c-c0xffffffff-rj-k-no'
+            }
+        ]
+    }
+]
