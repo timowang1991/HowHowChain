350a4747d14476d44662da9bc05f5f974fc10da7
Timo Wang <chiuanw@yahoo-inc.com>
2018-01-23 20:57:59 +0800

    add portfolio route

---
+++ src/layouts/kol/KolPortfolio.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class KolPortfolio extends Component {
+    render() {
+        return (
+            <div>I am the KolPortfolio</div>
+        );
+    }
+}
+
+export default KolPortfolio;
